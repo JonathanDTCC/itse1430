675678ce5f3614e1aef4a4ce90d8c2170c3d7a06
ZachHunter <zacharthunter@gmail.com>
2020-08-24 21:33:04 -0500

    Created Hello World

---
+++ classwork/itse1430.HelloWorld/itse1430.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace itse1430.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
