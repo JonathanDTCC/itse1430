d3df89d755f5c4ba09b4582c2a4470d4ad92023a
JonathanDTCC <jonathan.daniel749@my.tccd.edu>
2020-09-02 21:41:50 -0500

    More work on MovieLibrary

--- classwork/MovieLibrary/MovieLibrary/Program.cs
+++ classwork/MovieLibrary/MovieLibrary/Program.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿/*
+ * ITSE 1430
+ * Movie Library Console Application
+ */
+using System;
 
 namespace MovieLibrary
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
             //FunWithTypes();
             //FunWithVariable();
+
+            // while == while (E) S;
+            // 0+ iterations, pre test condition
+            while (true)
+            {
+                //Scope - lifetime of a variable : starts at declaration and continues until end of current scope
+                char choice = DisplayMenu();
+                if (choice == 'Q')
+                    return;
+                else if (choice == 'A')
+                    AddMovie();
+            };
 
             string title = "";
             string description = "";
@@ -15,11 +31,134 @@ namespace MovieLibrary
             int duration;
         }
 
+        static void AddMovie ()
+        {
+            //Get title
+            Console.WriteLine("Movie title: ");
+            //string title = Console.ReadLine();
+            string title = ReadString(true);
+
+            //Get description
+            Console.WriteLine("Description (optional): ");
+            //string description = Console.ReadLine();
+            string description = ReadString(false);
+
+            //Get rating
+            Console.WriteLine("Rating: ");
+            //string rating = Console.ReadLine();
+            string rating = ReadString(false);
+
+            //Get duration
+            Console.WriteLine("Duration (in minutes): ");
+            //string duration = Console.ReadLine();
+            int duration = ReadInt32(0);
+
+            //Get is classic
+            Console.WriteLine("Is Classic (Y/N)? ");
+            string isClassic = Console.ReadLine();
+        }
+
+        static char DisplayMenu ()
+        {
+            // 1+ iteration, post test
+            // do S while (E);
+            // block statement { S* }
+            do
+            {
+                Console.WriteLine("Movie Library");
+                Console.WriteLine("=================");
+
+                Console.WriteLine("A)dd Movie");
+                Console.WriteLine("Q)uit");
+
+                //Get input from user
+                string value = Console.ReadLine();
+
+                //C++; if (x = 10) ; //Not valid in C#
+                // if (E) S;
+                // if (E) S else S;
+                if (value == "Q") // 2 equal signs == equality
+                    return 'Q';
+                else if (value == "A")
+                    return 'A';
+
+                DisplayError("Invalid option");
+            } while (true);
+        }
+
+        private static void DisplayError ( string message )
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+
+        static int ReadInt32 ()
+        {
+            return ReadInt32(Int32.MinValue);
+        }
+        static int ReadInt32 ( int minimumValue )
+        {
+            do
+            {
+                string value = Console.ReadLine();
+
+                //Parse to int int Int32.Parse (string)
+                //int result = Int32.Parse(value);
+
+                // Parameter kinds
+                //  Input parameter ("pass by value") - a copy of the argument is placed into parameter inside function definition
+                //  Input/output parameter ("pass by reference") - a reference to the argument is passed to the function and both original argument and parameter reference same value (C++: int&)
+                //  Output parameter - function caller does not provide input, function always provides output (C++: return type)
+                //bool Int32.TryParse (string, out int result)
+                // Double.Parse/TryParse
+                // Single.Parse/TryParse
+                // Boolean.Parse/TryParse
+                // Int16.Parse/TryParse
+                int result;
+                if (Int32.TryParse(value, out result) && result >= minimumValue)
+                    return result;
+
+                if (minimumValue != Int32.MinValue)  //Int32.MaxValue
+                    DisplayError("Value must be at least " + minimumValue); //String concatentation
+                else
+                    DisplayError("Must be integral value");
+            } while (true);
+        }
+        //Logical operators
+        // NOT = !E : boolean
+        // AND => E && E : boolean
+        // OR => E || E : boolean
+
+        //Relational operators (primitives + a few extra)
+        // equality => E == E
+        // inquality => E != E
+        // greater than => E > E
+        // greater than or equal to => E >= E
+        // less than => E < E
+        // less than or equal to => E <= E
+        static string ReadString ( bool required )
+        {
+            do
+            {
+                string value = Console.ReadLine();
+
+                //If not required or not empty return
+                if (!required || value != "")
+                    return value;
+
+                DisplayError("Value is required");
+            } while (true);
+        }
+
         private static void FunWithVariable ()
         {
             //Definition before usage
             //Value;
-            
+
             //Declares hours of type int with initial value 10 (initializer expression)
             int hours = 10;
 
@@ -66,7 +205,7 @@ namespace MovieLibrary
         //Function definition - declaration + implementation
         // [modifiers] T id ([parameters]) { S* }
         //Function signature - [return-type] name, parameter types
-        static void FunWithTypes()
+        static void FunWithTypes ()
         {
             //Variable - named memory location where you can read and write a value; name, type and value
             //
@@ -93,7 +232,7 @@ namespace MovieLibrary
             //  0xFFUL ==> ulong or 0xFFU ==> uint
             //  decimal = 0-9, hex = 0-9A-F, binary = 0-1 (0b101100)
             //  32_766, 3_2_7_6_6, 0b1011_1010
-            
+
             //Variable decaration - T id [ = E ];
             int hours = 10;
             int code = 0xFF;
