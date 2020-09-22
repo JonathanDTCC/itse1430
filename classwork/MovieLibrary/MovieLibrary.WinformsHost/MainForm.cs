fef3fac605cd42aab23970b6473799a1f7e2b73b
JonathanDTCC <jonathan.daniel749@my.tccd.edu>
2020-09-21 21:49:35 -0500

    9-21-2020 Winforms and Movie.cs work

---
+++ classwork/MovieLibrary/MovieLibrary.WinformsHost/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System; //DO NOT DELETE
+using System.Windows.Forms;
+
+//Hierarchial namespaces
+//namespace MovieLibrary
+//{
+//    namespace WinformsHost
+//    {
+
+//    }
+//}
+//namespace Company.Product.<area>
+//namespace Microsoft.Office.Word
+    //namespace Microsoft.Office.Excel
+
+namespace MovieLibrary.WinformsHost
+{
+    public partial class MainForm : Form
+    {
+        public MainForm ()
+        {
+            InitializeComponent();
+
+            Movie movie;
+        }
+
+    }
+}
+
+//namespace OtherNamespace
+//{
+//    public class MainForm
+//    {
+
+//    }
+//}
